a9cf66ee36e1b7229f5d25c7a61525ee79d40d7f
amufaamo <amufaamo@gmail.com>
2026-02-16 21:54:30 +0900

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://amufaamo.github.io',
   base: '/astro',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
