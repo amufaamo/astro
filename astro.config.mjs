6e99c5d7684d94581798a610e16b5bacde567864
amufaamo <amufaamo@gmail.com>
2026-02-28 22:31:01 +0900

    Initial deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // あなたのURL: https://<ユーザー名>.github.io/<リポジトリ名>/
+    site: 'https://amufaamo.github.io',
+    base: '/astro', // リポジトリ名を / で始めて書く
+});
